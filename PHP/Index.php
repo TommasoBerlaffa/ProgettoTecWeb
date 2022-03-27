ee0da92ee8ebbd3da4c7e1e0a65e9aef49157aa3
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-03-27 19:57:30 +0200

    Fix Indentation and Links
    Piccole correzioni indentazione e sistemati alcuni link ( da .html a .php )

--- PHP/Index.php
+++ PHP/Index.php
@@ -2,23 +2,22 @@
 
   // Get HTML
   // HTML/Index.html
-  $url ='..'.  DIRECTORY_SEPARATOR  .'HTML'.  DIRECTORY_SEPARATOR  .'Index.html';
+  $url ='..'.DIRECTORY_SEPARATOR.'HTML'.DIRECTORY_SEPARATOR.'Index.html';
   $HTML = file_get_contents($url);
   session_start();
   $HTMLContent='';
   if(isset($_SESSION['user_Username']))
   {
-    $HTML = str_replace('{{CreateJob}}','<a href="..'. DIRECTORY_SEPARATOR .'HTML'. DIRECTORY_SEPARATOR .'CreateJob.html"> Create a Job Offer </a>',$HTML);
-      $HTMLContent = '<a href="..'. DIRECTORY_SEPARATOR .'PHP'. DIRECTORY_SEPARATOR .'UserProfile.php"> User Profile </a>
-        <img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR . $_SESSION['user_Icon'] .'" alt="Profile Picture" width="16" height="16">';
+    $HTML = str_replace('{{CreateJob}}','<a href="..'.DIRECTORY_SEPARATOR.'HTML'.DIRECTORY_SEPARATOR.'CreateJob.html"> Create a Job Offer </a>',$HTML);
+      $HTMLContent = '<a href="..'.DIRECTORY_SEPARATOR.'PHP'.DIRECTORY_SEPARATOR.'UserProfile.php"> User Profile </a>
+        <img src="..'.DIRECTORY_SEPARATOR.'IMG'.DIRECTORY_SEPARATOR. $_SESSION['user_Icon'] .'" alt="Profile Picture" id="icon">';
    
   }
   else
   {
     $HTML = str_replace('<li>{{CreateJob}}</li>','',$HTML);
-      $HTMLContent = '<a href="..'. DIRECTORY_SEPARATOR .'PHP'. DIRECTORY_SEPARATOR .'Login.php"> Login </a>
-                <span> or </span>
-                <a href="..'. DIRECTORY_SEPARATOR .'PHP'. DIRECTORY_SEPARATOR .'Signup.php"> Sign up </a>'; 
+      $HTMLContent = '<a href="..'.DIRECTORY_SEPARATOR.'PHP'.DIRECTORY_SEPARATOR.'Signup.php"> Sign up </a></li>
+                      <li class="right"><a href="..'.DIRECTORY_SEPARATOR.'PHP'.DIRECTORY_SEPARATOR.'Login.php"> Login </a>'; 
   }
 
   $HTML = str_replace('{{SubPage}}',$HTMLContent,$HTML);
