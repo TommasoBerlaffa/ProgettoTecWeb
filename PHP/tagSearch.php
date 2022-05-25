acf5bd454247632d52b9b2f423276ee283b8f2b8
Khaleeja <86421263+Khaleeja@users.noreply.github.com>
2022-05-25 11:15:10 +0200

    small simple search function

--- PHP/tagSearch.php
+++ PHP/tagSearch.php
@@ -1,44 +1,45 @@
 <?php
 
-	require_once 'DBAccess.php';
+require_once 'DBAccess.php';
 
-    session_start();
+session_start();
 	
-	const cap = 10;
+const cap = 10;
 	
-	$stamp_init = date("Y-m-d H:i:s");
-    if( !isset( $_SESSION['FIRST_REQUEST_TIME'] ) ){
-            $_SESSION['FIRST_REQUEST_TIME'] = $stamp_init;
-    }
-    $first_request_time = $_SESSION['FIRST_REQUEST_TIME'];
-    $stamp_expire = date( "Y-m-d H:i:s", strtotime( $first_request_time )+( 5 ) );
-    if( !isset( $_SESSION['REQ_COUNT'] ) ){
-            $_SESSION['REQ_COUNT'] = 0;
-    }
-    $req_count = $_SESSION['REQ_COUNT'];
-    $req_count++;
-    if( $stamp_init > $stamp_expire ){//Expired
-            $req_count = 1;
-            $first_request_time = $stamp_init;
-    }
-    $_SESSION['REQ_COUNT'] = $req_count;
-    $_SESSION['FIRST_REQUEST_TIME'] = $first_request_time;
-    header('X-RateLimit-Limit: '.cap);
-    header('X-RateLimit-Remaining: ' . ( cap-$req_count ) );
-    if( $req_count > cap){//Too many requests
-            http_response_code( 429 );
-            exit();
-    }
-	
-	$post = json_decode(file_get_contents('php://input'),true);
+$stamp_init = date("Y-m-d H:i:s");
+if( !isset( $_SESSION['FIRST_REQUEST_TIME'] ) ){
+        $_SESSION['FIRST_REQUEST_TIME'] = $stamp_init;
+}
+$first_request_time = $_SESSION['FIRST_REQUEST_TIME'];
+$stamp_expire = date( "Y-m-d H:i:s", strtotime( $first_request_time )+( 5 ) );
+if( !isset( $_SESSION['REQ_COUNT'] ) ){
+        $_SESSION['REQ_COUNT'] = 0;
+}
+$req_count = $_SESSION['REQ_COUNT'];
+$req_count++;
+if( $stamp_init > $stamp_expire ){//Expired
+        $req_count = 1;
+        $first_request_time = $stamp_init;
+}
+$_SESSION['REQ_COUNT'] = $req_count;
+$_SESSION['FIRST_REQUEST_TIME'] = $first_request_time;
+header('X-RateLimit-Limit: '.cap);
+header('X-RateLimit-Remaining: ' . ( cap-$req_count ) );
+if( $req_count > cap){//Too many requests
+        http_response_code( 429 );
+        exit();
+}
 
-	if(isset($post['TagsSearch'])) {
-        $word = filter_var($post['TagsSearch'], FILTER_SANITIZE_STRING);
-        if(strlen($word) == 0)
-            exit();
-		$DBAccess= new DBAccess();
-		$result=$DBAccess->searchTags($word);
-    foreach($result as $tag)
-			echo($tag);
-	}
+$post = json_decode(file_get_contents('php://input'),true);
+
+if(isset($post['TagsSearch'])) {
+    $word = filter_var($post['TagsSearch'], FILTER_SANITIZE_STRING);
+preg_replace("/[^A-Za-z ]/", "", $word);
+    if(strlen($word) == 0)
+        exit();
+$DBAccess= new DBAccess();
+$result=$DBAccess->searchTags($word);
+foreach($result as $tag)
+	echo($tag);
+}
 ?>
