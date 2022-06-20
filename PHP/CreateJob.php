fd3160e6ca78c3f9bfa9ea4ce5cb912286a4df3b
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-06-20 17:42:38 +0200

    Prime Modifiche Change Jobs (da Modificare)

--- PHP/CreateJob.php
+++ PHP/CreateJob.php
@@ -8,79 +8,14 @@
     if(isset($_SESSION['user_Username']))
     {
       $HTML = str_replace('<subpage/>','<li><a href="..'. DIRECTORY_SEPARATOR .'PHP'. DIRECTORY_SEPARATOR .'UserProfile.php">
-      <img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR . $_SESSION['user_Icon'] .'" alt="Profile Picture" id="profilepic" class="icons">User Profile</a></li>',$HTML);
-
+      <img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR.'usrprfl'. DIRECTORY_SEPARATOR . $_SESSION['user_Icon'] .'" alt="Profile Picture" id="profilepic" class="icons">User Profile</a></li>',$HTML);
     }
     else
     {
       $_SESSION['Url'] = 'CreateJob';
       header("location: ../PHP/Login.php");
     }
-  /*
-
-
-    //ID da ottenere tramite session
-    //$ID=$_SESSION['user_ID']; $
-    $Title='';    $Desc="";    $Tipology="";    $MinPay="";    $MaxPay="";    $Skills="";
-
-    if(isset($_SESSION['user_ID']))
-      $ID=$_SESSION('user_ID');
-    else
-      header("location: ../PHP/Login.php");
-
-
-    if(isset($_POST['Title']))
-      $Title=htmlentities(trim($_POST["Title"]));
-    if(isset($_POST['Desc']))
-      $Desc=htmlentities(trim($_POST["Desc"]));
-
-    if(isset($_POST['Tipology']))
-      $Tipology=htmlentities(trim($_POST["Tipology"]));
-
-    $Payment="0"; //????
-
-    if( isset($_POST['MinPay']))
-      $MinPay = filter_var($_POST['MinPay'], FILTER_VALIDATE_INT);
-
-    if( isset($_POST['MinPay']))
-      $MaxPay = filter_var($_POST['MaxPay'], FILTER_VALIDATE_INT);
-
-    if( isset($_POST['Skills']))
-      $Skills = $_POST['Skills']; //non così ma ok
-    $Skills="test";
-    $Expiring=date('Y-m-d H:i:s', strtotime('+7 days'));
-    $Result;
-
-
-
-    $url = '..'. DIRECTORY_SEPARATOR .'HTML'. DIRECTORY_SEPARATOR .'CreateJob.html';
-    $HTML = file_get_contents($url);
-
-    //Valutare di invertire i 2 rami then/else
-    if(isset($ID) && isset($Title) && isset($Desc) && isset($Tipology) && isset($MinPay) && isset($MaxPay) && isset($Skills) && isset($Expiring)){
-      if($MaxPay>=$MinPay){
-        $DBAccess = new DBAccess();
-			if(!($DBAccess->openDBConnection())){
-			header('Location:..'. DIRECTORY_SEPARATOR .'HTML'. DIRECTORY_SEPARATOR .'Error500.html');
-			exit;
-		}
-        $Result=$DBAccess->createJob($ID, $Title,$Desc,$Tipology,$Payment,$MinPay,$MaxPay,$Expiring); //Id e Payment???
-        $DBAccess->closeDBConnection();
-		if($Result)
-          $HTML = str_replace('<caricato/>','Job offer created!',$HTML); //Se si aggiorna la pagina dopo, si continua a caricare più volte lo stesso lavoro : Da fixare
-        else
-          $HTML = str_replace('<caricato/>','Error creating the job offer!!',$HTML);
-
-      }else{
-        $HTML = str_replace('<caricato/>','Max Pay must be greater than or equal to the Min Pay !!',$HTML);
-      }
-    }else{
-      //Qualche variabile non è settata, ritorna errore (?)
-      //$HTML = str_replace('<caricato/>','Error creating the job offer!!',$HTML);
-    }*/
-
 
     echo $HTML;
-
 
 ?>
--- PHP/NewJob.php
+++ PHP/NewJob.php
@@ -1,46 +1,43 @@
 <?php
-    require_once "ConnectionToDatabase.php";
-    use _Database\Database;
+    require_once "DBAccess.php";
+
     session_start();
 
+    if( !isset($_SESSION["user_Username"]))
+      header("Location:index.php");
+
     //ID da ottenere tramite session
-    $ID=$_SESSION("ID");
-      //Trim per togliere spazi e htmlentities per assicurarci che gli utenti non inseriscano tag html
-    $Title=htmlentities(trim($_POST["Title"]));
-    $Desc=htmlentities(trim($_POST["Desc"]));
-    $Tipology=htmlentities(trim($_POST["Tipology"]));
-    $Payment;
-    filter_var(INPUT_GET,'MaxPay',FILTER_SANITIZE_NUMBER_FLOAT);
-    filter_var(INPUT_GET,'MinPay',FILTER_SANITIZE_NUMBER_FLOAT);
-    //(l'utente può aver cambiato l'html quindi potrebbero non esserci floats)
-    $MinPay=htmlentities(trim($_POST["MinPay"]));
-    $MaxPay=htmlentities(trim($_POST["MaxPay"]));
-    $Skills=htmlentities(trim($_POST["Skills"])); //Questo non così ma ok
-    $Expiring=date('Y-m-d H:i:s', strtotime('+7 days')); //Da Controllare se funzia
+    $Id=$_SESSION['user_ID'];
 
-    $Result;
+    isset($_POST["Title"]) ? $Title = filter_var($_POST["Title"], FILTER_SANITIZE_STRING) : $Title="";
+    
+    isset($_POST["Description"]) ? $Desc = filter_var($_POST["Description"], FILTER_SANITIZE_STRING) : $Desc="";
+    
+    isset($_POST["Type"]) ? $Type= filter_var($_POST["Type"], FILTER_SANITIZE_STRING) : $Type="";
+    
+    isset($_POST["Min"]) ? $Min = filter_var($_POST["Min"], FILTER_SANITIZE_NUMBER_INT) : $Min="";
+    
+    isset($_POST["Max"]) ? $Max = filter_var($_POST["Max"], FILTER_SANITIZE_NUMBER_INT) : $Max="";
+    
+    isset($_POST["Pay"]) ? $Pay = filter_var($_POST["Pay"], FILTER_SANITIZE_NUMBER_INT) : $Pay="";
+    
+    $Expiring = date("Y-m-d", strtotime("+1 week"));
 
-
-    //Vari check che le variabili non abbiano valori strani/non validi etc
-    //Valutare di invertire i 2 rami then/else
-    if(!isset($ID) || !isset($Title) || !isset($Desc) || !isset($Tipology) || !isset($MinPay) || !isset($MaxPay) || !isset($Skills) || !isset($Expiring)){
-      //Qualche variabile non è settata, ritorna errore (?)
-    }else{
-      if($MaxPay<$MinPay) {
-        //Ritorna errore su questo
-        //header();
-      }
-
+    $DBAccess = new DBAccess();
+    if(!($DBAccess->openDBConnection())){
+      header('Location:..'. DIRECTORY_SEPARATOR .'HTML'. DIRECTORY_SEPARATOR .'Error500.html');
+      exit;
     }
-    //Apertura, caricamento e chiusura su db
-
-    $Result=createJob($ID, $Title,$Desc,$Tipology,$Payment,$MinPay,$MaxPay,$Expiring); //Id e Payment???
+    $Result=$DBAccess->createJob($Id, $Title,$Desc,$Type,$Pay,$Min,$Max,$Expiring); //Id e Payment???
+    $DBAccess->closeDBConnection();
     if($Result) {
       //Tutto ok, Si conferma il successo
     }
     else {
       //Errore, si dice che qualcosa non è andato come doveva
     }
+    
+
 
 
 ?>
