582b1bff170ef3b72a2e019abbfdcfc356fc3c37
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-06-21 15:28:48 +0200

    Small Fixes

--- PHP/AddFeedback.php
+++ PHP/AddFeedback.php
@@ -11,8 +11,9 @@ if(isset($_SESSION['user_Username'])) {
 	}
     // Controllo se nella sessione c'é User ID (dovrebbe esserci per il controllo di User Username ma è meglio fare 2 controlli)
     if(isset($_SESSION['user_ID'])) {
-		isset($_POST["star"]) ? $star =  $_POST["star"] : $star=null;
-		isset($_POST["comment"]) ? $comment =  $_POST["comment"] : $comment='';
+		isset($_POST["star"]) ? $star =  trim($_POST["star"]) : $star=null;
+		isset($_POST["comment"]) ? $comment =  trim($_POST["comment"]) : $comment='';
+
 		$date = date("Y/m/d");
 		if($DBAccess->createReview($_SESSION['user_ID'],$_SESSION["Code_job"],$star,$comment,$date)){
 			$DBAccess->closeDBConnection();
--- PHP/NewJob.php
+++ PHP/NewJob.php
@@ -9,33 +9,34 @@
     //ID da ottenere tramite session
     $Id=$_SESSION['user_ID'];
 
-    isset($_POST["Title"]) ? $Title = filter_var($_POST["Title"], FILTER_SANITIZE_STRING) : $Title="";
-    
-    isset($_POST["Description"]) ? $Desc = filter_var($_POST["Description"], FILTER_SANITIZE_STRING) : $Desc="";
-    
-    isset($_POST["Type"]) ? $Type= filter_var($_POST["Type"], FILTER_SANITIZE_STRING) : $Type="";
-    
-    isset($_POST["Min"]) ? $Min = filter_var($_POST["Min"], FILTER_SANITIZE_NUMBER_INT) : $Min="";
-    
-    isset($_POST["Max"]) ? $Max = filter_var($_POST["Max"], FILTER_SANITIZE_NUMBER_INT) : $Max="";
-    
-    isset($_POST["Pay"]) ? $Pay = filter_var($_POST["Pay"], FILTER_SANITIZE_NUMBER_INT) : $Pay="";
-    
-    $Expiring = date("Y-m-d", strtotime("+1 week"));
-
     $DBAccess = new DBAccess();
     if(!($DBAccess->openDBConnection())){
       header('Location:..'. DIRECTORY_SEPARATOR .'HTML'. DIRECTORY_SEPARATOR .'Error500.html');
       exit;
     }
-    $Result=$DBAccess->createJob($Id, $Title,$Desc,$Type,$Pay,$Min,$Max,$Expiring); //Id e Payment???
+
+    if( (isset($_POST["Title"]) && isset($_POST["Type"]) && isset($_POST["PayV"]) && isset($_POST["Description"]) && isset($_POST["Min"])) && isset($_POST["Max"]) )
+    {
+      $Title = filter_var($_POST["Title"], FILTER_SANITIZE_STRING);
+      $Desc = filter_var($_POST["Description"], FILTER_SANITIZE_STRING);
+      $Type= filter_var($_POST["Type"], FILTER_SANITIZE_STRING);
+      $Min = filter_var($_POST["Min"], FILTER_SANITIZE_NUMBER_INT);
+      $Max = filter_var($_POST["Max"], FILTER_SANITIZE_NUMBER_INT);
+      $Expiring = date("Y-m-d", strtotime("+1 week"));
+      $_POST["Max"] == 'Pay2' ? $Pay = 0 : $Pay = 1;
+    }
+    else
+      header("Location:CreateJob.php");
+    
+    $Result=$DBAccess->createJob($Id,$Title,$Desc,$Type,$Pay,$Min,$Max,$Expiring);
     $DBAccess->closeDBConnection();
-    if($Result) {
-      //Tutto ok, Si conferma il successo
-    }
+    if($Result) 
+      header("Location:UserProfile.php?section=2");
     else {
-      //Errore, si dice che qualcosa non è andato come doveva
+      header("Location:CreateJob.php?error=1");
     }
+      
+    
     
 
 
