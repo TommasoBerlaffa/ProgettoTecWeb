a578e7c30b091f7c3de96f35fbebbf3072074adf
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-07-03 18:07:17 +0200

    aggiunta alt

--- PHP/FindJob.php
+++ PHP/FindJob.php
@@ -15,18 +15,18 @@
 	if(isset($_SESSION['user_Username']))
 	{
 		$HTML = str_replace('<createjob/>','<li><a href="CreateJob.php">
-		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'write.png" class="icons"> Create an Offer </a></li>',$HTML);
+		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'write.png" class="icons" alt=""> Create an Offer </a></li>',$HTML);
 		$HTMLContent = '<li><a href="UserProfile.php">
-		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'UsrPrfl'. DIRECTORY_SEPARATOR . $_SESSION['user_Icon'] .'" alt="Profile Picture" id="profilepic" class="icons">User Profile</a></li>';
+		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'UsrPrfl'. DIRECTORY_SEPARATOR . $_SESSION['user_Icon'] .'" alt="Your Profile Picture" id="profilepic" class="icons">User Profile</a></li>';
 	}
 	else
 	{
 		$HTML = str_replace('<createjob/>','',$HTML);
 		$HTMLContent = '
 		<li><a href="Login.php">
-		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'login.png" class="icons"> Login </a></li>
+		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'login.png" class="icons" alt=""> Login </a></li>
 		<li><a href="Signup.php">
-		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'book.png" class="icons"> Sign up </a></li>';
+		<img src="..'. DIRECTORY_SEPARATOR .'IMG'. DIRECTORY_SEPARATOR .'Icons'. DIRECTORY_SEPARATOR .'book.png" class="icons" alt=""> Sign up </a></li>';
 	}
 	
 	// Cambio Pagina
