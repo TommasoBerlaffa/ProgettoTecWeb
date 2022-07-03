6dbb9e28798f8dc0add43ccffcf3e4cd37dac379
Khaleeja <Milospadotto96@gmail.com>
2022-07-03 22:56:15 +0200

    CSS per 404. NOn ne sono molto capace e va sistemato

--- JS/404.js
+++ JS/404.js
@@ -3,36 +3,41 @@ function fill(){
 	if(!div)
 		return;
 	
+	var cont=document.createElement('div');
+	cont.id='container404';
+	cont.classList.add("choose");
+	div.appendChild(cont);
+	
 	var bt=document.createElement('button');
-	bt.innerHTML="Moe Lester - Junior Java Developer.";
+	bt.innerHTML="Moe Lester<br>Junior Javascript Developer.";
 	bt.classList.add("btn404");
 	bt.setAttribute('type','button');
 	bt.setAttribute('onclick','thanks("Moe Lester")');
-	div.appendChild(bt);
+	cont.appendChild(bt);
 	
 	bt=document.createElement('button');
-	bt.innerHTML="Soshita Nakagata - Software quality assurance (QA) engineer";
+	bt.innerHTML="Soshita Nakagata<br>Software quality assurance (QA) engineer.";
 	bt.classList.add("btn404");
 	bt.setAttribute('type','button');
 	bt.setAttribute('onclick','thanks("Soshita Nakagata")');
-	div.appendChild(bt);
+	cont.appendChild(bt);
 	
 	bt=document.createElement('button');
-	bt.innerHTML="Chris P. Bacon - Responsible of Breakroom. (He once unplugged the server to cook a toast)";
+	bt.innerHTML="Chris P. Bacon<br>Full Sandwich Developer.";
 	bt.classList.add("btn404");
 	bt.setAttribute('type','button');
 	bt.setAttribute('onclick','thanks("Chris P. Bacon")');
-	div.appendChild(bt);
+	cont.appendChild(bt);
 	
 	bt=document.createElement('button');
-	bt.innerHTML="Ana L. Joy - Back-End developer.";
+	bt.innerHTML="Ana L. Joy<br>Back-End developer.";
 	bt.classList.add("btn404");
 	bt.setAttribute('type','button');
 	bt.setAttribute('onclick','thanks("Ana L. Joy")');
-	div.appendChild(bt);
+	cont.appendChild(bt);
 	
-	bt=document.createElement('paragaph');
-	bt.innerHTML="We are terribly sorry of this inconvinience. One of our employees have done another small opsie. Please, choose the name of the employee you think might be responsible for this.";
+	bt=document.createElement('div');
+	bt.innerHTML="<p>We are terribly sorry of this inconvinience. One of our employees have done another small opsie.<br>Please, choose the name of the employee you think might be responsible for this.<p>";
 	div.appendChild(bt);
 	
 }
@@ -41,7 +46,7 @@ function thanks(n){
 	var div=document.getElementById('choose');
 	if(!div)
 		return;
-	div.innerHTML='<p id="404-thanks">Thank you for your feedback! You chose ' + n + '. We are going to fire him/her immediatelly!</p>';
+	div.innerHTML='<p>Thank you for your feedback!<br>You chose ' + n + '.<br>We are going to fire him/her immediatelly!</p>';
 	
 	
 }
