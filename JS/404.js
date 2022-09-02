5bfc45ace7119f94d115135d28d1a236686bd5db
Khaleeja <Milospadotto96@gmail.com>
2022-09-02 23:24:27 +0200

    404 senza JS

--- JS/404.js
+++ JS/404.js
@@ -1,4 +1,14 @@
+var code='<div id="choose">'+
+		'<button type="button" class="btn404" value="Moe Lester">Moe Lester<br>Junior Javascript Developer.</button>'+
+		'<button type="button" class="btn404" value="Soshita Nakagata">Soshita Nakagata<br>Software quality assurance (QA) engineer.</button>'+
+		'<button type="button" class="btn404" value="Chris P. Bacon">Chris P. Bacon<br>Full Sandwich Developer.</button>'+
+		'<button type="button" class="btn404" value="Ana L. Joy">Ana L. Joy<br>Back-End developer.</button>'+
+		'<p>We are terribly sorry of this inconvinience. One of our employees have done another small opsie.<br>Please, choose the name of the employee you think might be responsible for this.<p>'+
+	'</div>';
+
+
 window.onload = function(){
+document.getElementById("main").innerHTML=document.getElementById("main").innerHTML+code;
 document.getElementById("choose").addEventListener('click',(evnt)=>{
 	if(evnt.target.nodeName === 'BUTTON')
 		thanks(evnt.target.value);
